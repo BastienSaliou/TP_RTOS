63e3d9d740afad2f378ef6b9c2f2cbe3e3fc9a52
BastienSaliou <bastien.saliou@gmail.com>
2025-01-15 17:39:20 +0100

    structure + comm 2

--- TP_TempsReel/mux_demux.c
+++ TP_TempsReel/mux_demux.c
@@ -5,10 +5,8 @@
 
 #define TRAME_SIZE 14
 
-
-
+// Mélange aléatoire d'un tableau d'entiers
 void shuffle(int *array, int n) {
-
     for (int i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         int temp = array[i];
@@ -17,13 +15,12 @@ void shuffle(int *array, int n) {
     }
 }
 
-
-
 void multiplex(uint8_t *trame, ChannelData x, ChannelData y, ChannelData z, Status status) {
     int i = 0;
     int order[4] = {0, 1, 2, 3};
-    shuffle(order, 4);
+    shuffle(order, 4); // Mélange aléatoire des éléments
 
+    // Ajout des données dans la trame selon l'ordre aléatoire
     for (int k = 0; k < 4; k++) {
         switch (order[k]) {
             case 0:
@@ -49,41 +46,35 @@ void multiplex(uint8_t *trame, ChannelData x, ChannelData y, ChannelData z, Stat
     }
 }
 
-
 void demultiplex(uint8_t *trame, ChannelData *x, ChannelData *y, ChannelData *z, Status *status) {
     int i = 0;
 
-    // Parcourir la trame pour identifier et extraire chaque voie
+    // Parcours de la trame pour identifier et extraire chaque élément
     while (i < TRAME_SIZE) {
         uint8_t header = trame[i++];
         switch (header) {
-            case 0x01: // Voie X
+            case 0x01:
                 x->header = header;
                 memcpy(x->data, &trame[i], sizeof(x->data));
                 i += sizeof(x->data);
                 break;
 
-            case 0x02: // Voie Y
+            case 0x02:
                 y->header = header;
                 memcpy(y->data, &trame[i], sizeof(y->data));
-
                 i += sizeof(y->data);
                 break;
 
-            case 0x03: // Voie Z
+            case 0x03:
                 z->header = header;
                 memcpy(z->data, &trame[i], sizeof(z->data));
-
                 i += sizeof(z->data);
                 break;
 
-            case 0x04: // Status
+            case 0x04:
                 status->header = header;
                 status->status = trame[i++];
                 break;
-
         }
     }
 }
-
-
